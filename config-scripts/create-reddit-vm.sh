006d5e6629e11608cba07ee5ba0e0f6b0f622289
Ilya Fedotov <ilya.v.fedotov@gmail.com>
2020-06-28 21:57:45 +0300

    fixing errors 2

--- config-scripts/create-reddit-vm.sh
+++ config-scripts/create-reddit-vm.sh
@@ -5,3 +5,4 @@ yc compute instance create \
 --create-boot-disk name=disk1,size=20GB,image-name=reddit-full-1593369115 \
 --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
 --metadata serial-port-enable=1
+
