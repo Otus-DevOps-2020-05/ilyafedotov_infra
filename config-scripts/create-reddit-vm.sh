005d48dafdd72c97174cc3a82754fbd8ace82ce3
Ilya Fedotov <ilya.v.fedotov@gmail.com>
2020-06-28 21:47:19 +0300

    HomeWork - 7

---
+++ config-scripts/create-reddit-vm.sh
@@ -0,0 +1,7 @@
+yc compute instance create \
+--name reddit-full-app1 \
+--hostname reddit-full-app1 \
+--memory=4 \
+--create-boot-disk name=disk1,size=20GB,image-name=reddit-full-1593369115 \
+--network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
+--metadata serial-port-enable=1
